472759bfdeb7170da7e8786ef21b457682963348
Jess Scholtes <jess.scholtes@jscholtes.eu>
2019-01-09 11:56:39 +0100

    added flags for pid and config

--- main.go
+++ main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
 func main() {
-	customPath := ""
-	if len(os.Args) == 2 {
-		customPath = os.Args[1]
+
+	config := flag.String("f", "/usr/local/etc/DO-dyndns.json", "JSON Config file")
+	pid := flag.String("p", "", "Pid File")
+
+	flag.Parse()
+
+	if *pid != "" {
+		writePidFile(*pid)
 	}
-	setting, err := loadSettings(customPath)
+
+	setting, err := loadSettings(*config)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -70,4 +79,25 @@ func getIP() (string, error) {
 	}
 
 	return ip.String(), err
+}
+
+// Write a pid file, but first make sure it doesn't exist with a running pid.
+func writePidFile(pidFile string) error {
+	// Read in the pid file as a slice of bytes.
+	if piddata, err := ioutil.ReadFile(pidFile); err == nil {
+		// Convert the file contents to an integer.
+		if pid, err := strconv.Atoi(string(piddata)); err == nil {
+			// Look for the pid in the process list.
+			if process, err := os.FindProcess(pid); err == nil {
+				// Send the process a signal zero kill.
+				if err := process.Signal(syscall.Signal(0)); err == nil {
+					// We only get an error if the pid isn't running, or it's not ours.
+					return fmt.Errorf("pid already running: %d", pid)
+				}
+			}
+		}
+	}
+	// If we get here, then the pidfile didn't exist,
+	// or the pid in it doesn't belong to the user running this app.
+	return ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664)
 }
